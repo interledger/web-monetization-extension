98d6d5c00712a9f0dfcfafb07c2bcde2f4d84507
Ionut Anin <ionutanin@gmail.com>
2023-08-22 16:13:53 +0300

    creating custom events

--- src/lib/listeners.ts
+++ src/lib/listeners.ts
@@ -12,6 +12,24 @@ export const initListeners = () => {
 
     return true
   })
+
+  if (monetization) {
+    const paymentPointer = monetization.getAttribute('href') || ''
+    const customEvent = new CustomEvent('monetizationStream', {
+      detail: {
+        paymentPointer,
+      },
+    })
+
+    monetization.addEventListener('monetizationStream', event => {
+      console.log('monetizationStream', event)
+    })
+
+    setInterval(() => {
+      console.log('dispatching event')
+      document.dispatchEvent(customEvent)
+    }, 1000)
+  }
 }
 
 // eslint-disable-next-line no-unused-vars
