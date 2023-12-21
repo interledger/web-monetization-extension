a4c7f9c6edb0d48220e6d0027a9a8dd0520b9c4d
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-12-21 09:57:49 +0200

    Update jest config and setup files

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+export default {
+  preset: 'ts-jest',
+  clearMocks: true,
+  displayName: 'WM Extension',
+  collectCoverageFrom: [
+    'src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/**/*.css',
+    '!src/**/*.svg',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  maxWorkers: '50%',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/jest.config.ts'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '\\.(css|less|scss|sass|svg)$': 'jest-transform-stub',
+  },
+}
