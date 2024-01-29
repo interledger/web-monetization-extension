fe8f4cfc92a224b92fa04470fa606958762fa54a
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-01-29 08:41:55 +0200

    UI Bits

--- src/popup/pages/Home.tsx
+++ src/popup/pages/Home.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { runtime } from 'webextension-polyfill'
 
 import { sendMessage, sendMessageToActiveTab } from '@/utils/sendMessages'
+import { Input } from '@/components/input'
+import { Label } from '@/components/label'
+import { DollarSign } from '@/components/icons'
+import { Button } from '@/components/button'
 
 const Success = runtime.getURL('assets/images/web-monetization-success.svg')
 const Fail = runtime.getURL('assets/images/web-monetization-fail.svg')
@@ -118,52 +122,53 @@ export const Home = () => {
           ${spent}/<span>$20</span>
         </div>
       )}
-      <div className="content">
+      <div className="mb-20">
         {isMonetizationReady ? (
           <>
-            <img src={Success} alt="Success" />
-
-            <form
-              onSubmit={handleSubmit}
-              className={`pointerForm ${paymentStarted ? 'active' : ''}`}>
-              <div className="input-wrapper">
-                <label htmlFor="paymentPointer">Payment pointer</label>
-                <div className="input">
-                  <input
-                    type="text"
-                    name="paymentPointer"
-                    value={formData.paymentPointer}
-                    onInput={handleChange}
-                    disabled={paymentStarted}
-                    placeholder="https://ilp.rafiki.money/alice"
-                  />
-                </div>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-5">
+                <Label className="text-base" htmlFor="paymentPointer">
+                  Wallet address
+                </Label>
+                <Input
+                  id="paymentPointer"
+                  name="paymentPointer"
+                  onInput={handleChange}
+                  disabled={paymentStarted}
+                  placeholder="https://ilp.rafiki.money/alice"
+                />
               </div>
+              <Label className="text-base" htmlFor="amount">
+                Amount
+              </Label>
+              <Input
+                id="amount"
+                name="amount"
+                icon={<DollarSign />}
+                onInput={handleChange}
+                disabled={paymentStarted}
+                placeholder="20.00"
+              />
 
-              <div className="input-wrapper">
-                <label htmlFor="pointer">Amount</label>
-                <div className="input">
-                  <img src={DollarIcon} alt="dollar" />
-                  <input
-                    type="text"
-                    name="amount"
-                    value={formData.amount}
-                    onInput={handleChange}
-                    disabled={paymentStarted}
-                    placeholder="0.05"
-                  />
-                </div>
-              </div>
-
-              <div className="actions">
+              <div className="mt-5 w-full">
                 {paymentStarted ? (
-                  <button type="button" className="stop-btn" onClick={stopPayments}>
-                    <img src={CloseIcon} alt="Stop" />
-                  </button>
+                  <Button
+                    className="text-base"
+                    fullWidth
+                    aria-label="stop"
+                    type="button"
+                    onClick={stopPayments}>
+                    Disconnect
+                  </Button>
                 ) : (
-                  <button type="submit" className={`submit-btn ${loading ? 'loading' : ''}`}>
-                    <img src={CheckIcon} alt="Check" />
-                  </button>
+                  <Button
+                    fullWidth
+                    className="text-base"
+                    type="submit"
+                    aria-label="connect"
+                    loading={loading}>
+                    Connect
+                  </Button>
                 )}
               </div>
             </form>
