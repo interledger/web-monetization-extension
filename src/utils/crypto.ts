0426a5f8dcdae3b16bd6c49ab40710e00225fbb2
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-02-20 20:11:07 +0200

    Add comments

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -5,23 +5,12 @@ ed.etc.sha512Sync = (...m) => sha512(ed.etc.concatBytes(...m))
 
 export function generateEd25519KeyPair() {
   const rawPrivateKey = ed.utils.randomPrivateKey()
+  // PKCS#8 format (version + algorithm)
+  // Adding these values upfront solves the future import of the key using
+  // `crypto.subtle.importKey` once the WebCrypto API supports the Ed25519 algorithm.
+  // prettier-ignore
   const privateKey = new Uint8Array([
-    48,
-    46,
-    2,
-    1,
-    0,
-    48,
-    5,
-    6,
-    3,
-    43,
-    101,
-    112,
-    4,
-    34,
-    4,
-    32,
+    48, 46, 2, 1, 0, 48, 5, 6, 3, 43, 101, 112, 4, 34, 4, 32,
     ...rawPrivateKey,
   ])
   const publicKey = ed.getPublicKey(rawPrivateKey)
@@ -33,6 +22,8 @@ export function exportJWK(key: Uint8Array, kid: string) {
   const string = String.fromCharCode.apply(null, key)
 
   const base64 = btoa(string)
+  // Based on the JWK Spec - base64url encoded.
+  // https://datatracker.ietf.org/doc/html/rfc7517#section-3
   const base64Url = base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
 
   return {
