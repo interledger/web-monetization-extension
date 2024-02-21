9d10c2f4714fb13fb26e8007a0c3227e4f67ec4f
Ionut Anin <ionutanin@gmail.com>
2024-02-21 14:20:32 +0200

    remove unused mock

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,16 +2,4 @@ import '@testing-library/jest-dom'
 
 import { chrome } from 'jest-chrome'
 
-class ResizeObserverMock {
-  constructor(callback: ResizeObserverCallback) {
-    this.callback = callback
-  }
-  observe(): void {}
-  unobserve(): void {}
-  disconnect(): void {}
-  callback: ResizeObserverCallback
-}
-
-global.ResizeObserver = ResizeObserverMock
-
 Object.assign(global, { chrome: chrome, browser: chrome })
