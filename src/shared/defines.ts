528be01ca6aad96daa59ae6beaa23f1b306d284f
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-04-03 06:19:37 +0300

    chore: update scripts (#184)
    * Initial `dev` script and webpack config update
    
    * Update build and profiler scripts
    
    * Revert tsconfig paths
    
    * Update ZIP path
    
    * Use ROOT_DIR
    
    * Do not mangle and disable debug logging in production build

---
+++ src/shared/defines.ts
@@ -0,0 +1,5 @@
+import type { LogLevelDesc } from 'loglevel'
+
+declare const CONFIG_LOG_LEVEL: LogLevelDesc
+
+export const LOG_LEVEL = CONFIG_LOG_LEVEL
---
+++ webpack/prod.ts
@@ -0,0 +1,53 @@
+import { Configuration, DefinePlugin } from 'webpack'
+import { DIRECTORIES, ROOT_DIR, Target, mainConfig } from './config'
+import path from 'node:path'
+import { getMainPlugins } from './plugins'
+import TerserPlugin from 'terser-webpack-plugin'
+import ZipPlugin from 'zip-webpack-plugin'
+
+export const getProdConfig = (target: Target): Configuration => {
+  return {
+    ...mainConfig,
+    output: {
+      path: path.resolve(ROOT_DIR, `${DIRECTORIES.DIST}/${target}`),
+      filename: '[name]/[name].js',
+      clean: true
+    },
+    mode: 'production',
+    devtool: undefined,
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          terserOptions: {
+            mangle: false,
+            format: {
+              comments: false
+            }
+          },
+          extractComments: false
+        })
+      ]
+    },
+    plugins: getMainPlugins(DIRECTORIES.DIST, target).concat([
+      new ZipPlugin({
+        path: path.resolve(ROOT_DIR, `${DIRECTORIES.DIST}`),
+        filename: target,
+        extension: 'zip',
+        fileOptions: {
+          mtime: new Date(),
+          mode: 0o100664,
+          compress: true,
+          forceZip64Format: false
+        },
+        zipOptions: {
+          forceZip64Format: false
+        }
+      }),
+      new DefinePlugin({
+        CONFIG_LOG_LEVEL: JSON.stringify('WARN')
+      })
+    ])
+  }
+}
