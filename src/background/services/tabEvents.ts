365c2b294ccdf5721c0e85405179d6fa245d786d
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-04-18 10:55:29 +0300

    Add tab change handler

--- src/background/services/tabEvents.ts
+++ src/background/services/tabEvents.ts
@@ -7,7 +7,10 @@ export class TabEvents {
     this.monetizationService.clearTabSessions(tabId)
   }
 
-  // onUpdatedTab = (tabId: number) => {
-  //   this.monetizationService.clearTabSessions(tabId)
-  // }
+  // TODO: This is not ideal. Find a better way to clear the sessions for a specific tab.
+  // When closing the tab, we receive the STOP_MONETIZATION message as well.
+  // Maybe check if the tab is closed in the content script?
+  onUpdatedTab = (tabId: number) => {
+    this.monetizationService.clearTabSessions(tabId)
+  }
 }
