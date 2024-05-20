a43a361485f56baf1f2e90f75e64f46a98d3a5a9
Diana Fulga <fulga.dd@gmail.com>
2024-05-21 00:54:41 +0300

    Add unit tests

--- src/popup/lib/utils.test.ts
+++ src/popup/lib/utils.test.ts
@@ -40,15 +40,21 @@ describe('formatNumber', () => {
     expect(res).toEqual('0.0009')
 
     res = formatNumber(0.000000009, 9)
-    expect(res).toEqual('9e-9')
+    expect(res).toEqual('0.000000009')
 
     res = formatNumber(0.00009, 9)
+    expect(res).toEqual('0.00009')
+
+    res = formatNumber(0.000000009, 9, true)
+    expect(res).toEqual('9e-9')
+
+    res = formatNumber(0.00009, 9, true)
     expect(res).toEqual('9e-5')
 
-    res = formatNumber(0.0000109, 9)
+    res = formatNumber(0.0000109, 9, true)
     expect(res).toEqual('1.09e-5')
 
-    res = formatNumber(0.000010009, 9)
+    res = formatNumber(0.000010009, 9, true)
     expect(res).toEqual('1.0009e-5')
 
     res = formatNumber(0.000100009, 9)
