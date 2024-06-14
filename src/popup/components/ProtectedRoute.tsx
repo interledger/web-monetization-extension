ce97e717c965b0be460a5b42b6be904db2f2b0d7
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-06-14 17:36:06 +0530

    fix(popup): correctly check connected state to prevent crash

--- src/popup/components/ProtectedRoute.tsx
+++ src/popup/components/ProtectedRoute.tsx
@@ -9,7 +9,7 @@ export const ProtectedRoute = () => {
   if (!state.hasHostPermissions) {
     return <Navigate to={ROUTES_PATH.MISSING_HOST_PERMISSION} />
   }
-  if (state.connected === false) {
+  if (!state.connected) {
     return <Navigate to={ROUTES_PATH.SETTINGS} />
   }
 
