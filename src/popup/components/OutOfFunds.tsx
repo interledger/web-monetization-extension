4dd7c9e5b746a2139c5f15ee529e0109c98037b8
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-07-18 15:25:29 +0530

    add message for recurring auto-renew

--- src/popup/components/OutOfFunds.tsx
+++ src/popup/components/OutOfFunds.tsx
@@ -4,6 +4,7 @@ import type { RecurringGrant, OneTimeGrant } from '@/shared/types'
 import type { ConnectWalletPayload, Response } from '@/shared/messages'
 import type { WalletAddress } from '@interledger/open-payments'
 import { getCurrencySymbol, transformBalance } from '../lib/utils'
+import { getNextOccurrence } from '@/shared/helpers'
 
 interface OutOfFundsProps {
   info: Pick<WalletAddress, 'id' | 'assetCode' | 'assetScale'>
@@ -51,8 +52,33 @@ export const OutOfFunds = ({
       <h3 className="text-lg">
         Top-up: <span>{currencySymbol + amount}</span>
       </h3>
+      <RecurringAutoRenewInfo grantRecurring={grantRecurring} info={info} />
       <Button onClick={() => requestTopUpRecurring()}>Recurring</Button>
       <Button onClick={() => requestTopUpOneTime()}>One-time</Button>
     </div>
+  )
+}
+
+function RecurringAutoRenewInfo({
+  grantRecurring,
+  info
+}: Pick<OutOfFundsProps, 'grantRecurring' | 'info'>) {
+  if (!grantRecurring) return null
+
+  const currencySymbol = getCurrencySymbol(info.assetCode)
+  const amount = transformBalance(grantRecurring.value, info.assetScale)
+  const renewDate = getNextOccurrence(grantRecurring.interval, new Date())
+
+  return (
+    <p>
+      If you do nothing, you will have {currencySymbol}
+      {amount} available on{' '}
+      <time dateTime={renewDate.toISOString()}>
+        {renewDate.toLocaleString(undefined, {
+          dateStyle: 'long',
+          timeStyle: 'medium'
+        })}
+      </time>
+    </p>
   )
 }
