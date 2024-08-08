0f2ada21e966acffa33e10bb5aaa53337d6b30aa
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-08-08 14:52:08 +0530

    add comments; nits

--- src/background/services/heartbeat.ts
+++ src/background/services/heartbeat.ts
@@ -4,37 +4,33 @@ export class Heartbeat {
   constructor(private browser: Browser) {}
 
   start() {
+    const alarms = this.browser.alarms
+    // The minimum supported cross-browser period is 1 minute. So, we create 4
+    // alarms at a 0,15,30,45 seconds delay. So, we'll get an alarm every 15s -
+    // and that'll help us keep the background script alive.
+
+    // Note that the first alarm will trigger after a minute, but this maybe
+    // fine for our use case, as we may have enough events between start and
+    // first minute that our extension stays alive.
     setTimeout(
-      () =>
-        this.browser.alarms.create('keep-alive-alarm-0', {
-          periodInMinutes: 1
-        }),
+      () => alarms.create('keep-alive-alarm-0', { periodInMinutes: 1 }),
       0
     )
     setTimeout(
-      () =>
-        this.browser.alarms.create('keep-alive-alarm-1', {
-          periodInMinutes: 1
-        }),
+      () => alarms.create('keep-alive-alarm-1', { periodInMinutes: 1 }),
       15 * 1000
     )
     setTimeout(
-      () =>
-        this.browser.alarms.create('keep-alive-alarm-2', {
-          periodInMinutes: 1
-        }),
+      () => alarms.create('keep-alive-alarm-2', { periodInMinutes: 1 }),
       30 * 1000
     )
     setTimeout(
-      () =>
-        this.browser.alarms.create('keep-alive-alarm-3', {
-          periodInMinutes: 1
-        }),
+      () => alarms.create('keep-alive-alarm-3', { periodInMinutes: 1 }),
       45 * 1000
     )
 
-    this.browser.alarms.onAlarm.addListener((ev) => {
-      console.count('keeping extension alive - ' + ev.name)
+    alarms.onAlarm.addListener(() => {
+      // doing nothing is enough to keep it alive
     })
   }
 
