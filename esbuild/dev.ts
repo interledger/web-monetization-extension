193dfcb1541a8fcd02eb6088bfea2ba4bc2c283b
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-08-14 15:32:46 +0530

    Revert "Trying web-ext for reloading"
    This reverts commit d1856e54491e2158b337d359072c88d1809efa9d.

--- esbuild/dev.ts
+++ esbuild/dev.ts
@@ -1,8 +1,7 @@
-import type { BuildOptions, Plugin as ESBuildPlugin } from 'esbuild'
+import type { BuildOptions } from 'esbuild'
 import type { BuildArgs } from './config'
 import { getPlugins } from './plugins'
 import { typecheckPlugin } from '@jgoz/esbuild-plugin-typecheck'
-import webExt from 'web-ext'
 
 export const getDevOptions = ({
   outDir,
@@ -27,38 +26,7 @@ export const getDevOptions = ({
       )
     },
     plugins: getPlugins({ outDir, dev: true, target, channel }).concat([
-      typecheckPlugin({ buildMode: 'readonly', watch: true }),
-      liveReload({ outDir })
+      typecheckPlugin({ buildMode: 'readonly', watch: true })
     ])
-  }
-}
-
-function liveReload({outDir}): ESBuildPlugin {
-  return {
-    name: 'liveReload',
-    setup(build) {
-      webExt.cmd
-        .run(
-          {
-            // These are command options derived from their CLI conterpart.
-            // In this example, --source-dir is specified as sourceDir.
-            sourceDir: outDir
-          },
-          {
-            // These are non CLI related options for each function.
-            // You need to specify this one so that your NodeJS application
-            // can continue running after web-ext is finished.
-            shouldExitProgram: false
-          }
-        )
-        .then((extensionRunner) => {
-          // The command has finished. Each command resolves its
-          // promise with a different value.
-          console.log(extensionRunner)
-          // You can do a few things like:
-          // extensionRunner.reloadAllExtensions();
-          // extensionRunner.exit();
-        })
-    }
   }
 }
