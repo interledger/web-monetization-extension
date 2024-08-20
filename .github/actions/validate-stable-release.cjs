cb439742035d694d1d1da38f94f5a43e63409571
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-08-20 17:15:42 +0530

    add release-stable workflow
    Co-authored-by: Radu-Cristian Popa <radu.popa@breakpointit.eu>

---
+++ .github/actions/validate-stable-release.cjs
@@ -0,0 +1,36 @@
+// @ts-check
+
+/**
+ * 1. Validate input version.
+ * 2. Check if given tag/release is already promoted to stable. If so, crash.
+ * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
+ */
+module.exports = async ({ github, context }) => {
+  const { owner, repo } = context.repo
+  const previewVersionTag = process.env.INPUT_VERSION
+  if (!previewVersionTag) {
+    throw new Error('Missing env.INPUT_VERSION')
+  }
+  if (!previewVersionTag.match(/^v[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+-preview$/)) {
+    throw new Error('Input "version" must match vX.X.X.X-preview')
+  }
+
+  const versionTag = previewVersionTag.replace('-preview', '')
+  try {
+    await github.rest.repos.getReleaseByTag({
+      owner,
+      repo,
+      tag: versionTag
+    })
+    throw new Error('Release already promoted to stable')
+  } catch (error) {
+    if (!error.status) {
+      throw error
+    }
+    if (error.status === 404) {
+      // do nothing
+    } else {
+      throw new Error(`Failed to check: HTTP ${error.status}`, { cause: error })
+    }
+  }
+}
