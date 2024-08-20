81db8681f4eac3668acb812c4f7eff54678f38d9
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-08-20 11:22:02 +0300

    chore(ci): fix manifest path in get-built-version action (#519)
    * Fix manifest path
    
    * Update release body
    
    Co-authored-by: Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>

--- .github/actions/get-built-version.cjs
+++ .github/actions/get-built-version.cjs
@@ -1,6 +1,5 @@
 // @ts-check
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-var-requires */
-const path = require('node:path')
 const fs = require('node:fs/promises')
 
 /**
@@ -8,8 +7,9 @@ const fs = require('node:fs/promises')
  * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
  */
 module.exports = async ({ core }) => {
-  const manifestPath = path.join(__dirname, 'dist', 'chrome', 'manifest.json')
-  const manifest = await fs.readFile(manifestPath, 'utf8').then(JSON.parse)
+  const manifest = await fs
+    .readFile('./dist/chrome/manifest.json', 'utf8')
+    .then(JSON.parse)
 
   core.setOutput('version', manifest.version)
 }
