a44175264a8c7f945818484e37f7457cd18b78f2
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-08-20 13:34:44 +0300

    Add bump manifest version action and workflow
    Co-authored-by: Sid Vishnoi
    <8426945+sidvishnoi@users.noreply.github.com>

---
+++ .github/actions/bump-manifest-version.cjs
@@ -0,0 +1,47 @@
+// @ts-check
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-console */
+const fs = require("node:fs/promises");
+
+/** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+module.exports = async ({ core }) => {
+  const manifestPath = "./src/manifest.json";
+  const manifestFile = await fs.readFile(manifestPath, "utf8");
+  const manifest = JSON.parse(manifestFile);
+  /**@type {string} */
+  const existingVersion = manifest.version;
+
+  const bumpType = /** @type {BumpType} */ (process.env.INPUT_VERSION);
+  if (!bumpType) {
+    throw new Error("Missing bump type");
+  }
+
+  const version = bumpVersion(existingVersion, bumpType).join(".");
+
+  console.log({ existingVersion, bumpType, version });
+
+  manifest.version = version;
+  await fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2));
+  core.setOutput("version", version);
+};
+
+/**
+ * @typedef {'build' | 'patch' | 'minor'} BumpType
+ * @param {string} existingVersion
+ * @param {BumpType} type
+ * @return {[major: number, minor: number, patch: number, build: number]}
+ */
+function bumpVersion(existingVersion, type) {
+  const [major, minor, patch, build] = existingVersion.split(".").map(Number);
+
+  switch (type) {
+    case "build":
+      return [major, minor, patch, build + 1];
+    case "patch":
+      return [major, minor, patch + 1, 0];
+    case "minor":
+      return [major, minor + 1, 0, 0];
+    default:
+      throw new Error("Unknown bump type: " + type);
+  }
+}
