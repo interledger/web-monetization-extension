33057bcb1a432d0738539079f0eb88e7f33aba14
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-08-20 10:56:02 +0300

    Refactor version logic for nightly workflow
    Co-authored-by: Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>

---
+++ .github/actions/get-built-version.cjs
@@ -0,0 +1,16 @@
+// @ts-check
+/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-var-requires */
+const path = require('node:path')
+const fs = require('node:fs/promises')
+
+/** * Retrieves the manifest version from the built extension.
+    *
+    * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
+    * */
+module.exports = async ({ core }) => {
+  // Retrieves the manifest version from the built extension.
+  const manifestPath = path.join(__dirname, 'dist', 'chrome', 'manifest.json')
+  const manifest = await fs.readFile(manifestPath, 'utf8').then(JSON.parse)
+
+  core.setOutput('version', manifest.version)
+}
