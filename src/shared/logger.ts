bb097fb3ded6d27ecb2ac94e754e3bdc56ce91e7
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-08-28 17:11:53 +0530

    fix(shared/logger): log with correct line number in devtools

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -5,21 +5,6 @@ export const createLogger = (level: log.LogLevelDesc = 'DEBUG') => {
   /** This disables all logging below the given level,  */
   log.setLevel(level, false);
 
-  const factory = log.methodFactory;
-  log.methodFactory = (methodName, logLevel, loggerName) => {
-    const raw = factory(methodName, logLevel, loggerName);
-
-    return function (...args) {
-      const messages = [
-        `%c${loggerName as string}`,
-        'font-weight: bold; text-transform: uppercase; background: #2f8785; padding-inline: 5px;',
-        ...args,
-      ];
-      raw(...messages);
-    };
-  };
-  log.rebuild();
-
   return log;
 };
 
