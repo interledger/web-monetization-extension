857bc024cfa13ba10ba523d8c24f0c2ce23dab5c
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-08-30 19:52:34 +0530

    don't open html test report at end

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['list'], ['html']],
+  reporter: [['list'], ['html', { open: 'never' }]],
   use: {
     trace: 'on-first-retry',
   },
