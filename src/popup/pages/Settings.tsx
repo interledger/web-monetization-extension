14f8f192f9d8c543af06e864356ad4af237ee695
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-09-11 15:03:59 +0530

    refactor(popup/ConnectWalletForm): add props to make it more testable

--- src/popup/pages/Settings.tsx
+++ src/popup/pages/Settings.tsx
@@ -1,14 +1,36 @@
+import React from 'react';
 import { ConnectWalletForm } from '@/popup/components/ConnectWalletForm';
 import { WalletInformation } from '@/popup/components/WalletInformation';
-import { usePopupState } from '@/popup/lib/context';
-import React from 'react';
+import { useMessage, usePopupState } from '@/popup/lib/context';
+import { getWalletInformation } from '@/shared/helpers';
 
 export const Component = () => {
   const { state } = usePopupState();
+  const message = useMessage();
 
   if (state.connected) {
     return <WalletInformation info={state} />;
   } else {
-    return <ConnectWalletForm publicKey={state.publicKey} />;
+    return (
+      <ConnectWalletForm
+        publicKey={state.publicKey}
+        defaultValues={{
+          recurring:
+            localStorage?.getItem('connect.recurring') === 'true' || false,
+          amount: localStorage?.getItem('connect.amount') || undefined,
+          walletAddressUrl:
+            localStorage?.getItem('connect.walletAddressUrl') || undefined,
+        }}
+        onChange={(key, val) => {
+          localStorage?.setItem(`connect.${key}`, val.toString());
+        }}
+        getWalletInfo={getWalletInformation}
+        connectWallet={(data) => message.send('CONNECT_WALLET', data)}
+        onConnect={() => {
+          // The popup closes due to redirects on connect, so we don't need to
+          // update any state manually.
+        }}
+      />
+    );
   }
 };
