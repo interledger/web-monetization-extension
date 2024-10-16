e5f238ce916283a8eabd7ac2d6183b747db9e980
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-10-16 17:01:09 +0530

    refactor(popup): extract InputAmount from ConnectWalletForm (#672)

---
+++ src/popup/components/InputAmount.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import { Input } from './ui/Input';
+import type { WalletAddress } from '@interledger/open-payments';
+import { charIsNumber, formatNumber, getCurrencySymbol } from '../lib/utils';
+import {
+  errorWithKey,
+  ErrorWithKeyLike,
+  formatCurrency,
+} from '@/shared/helpers';
+
+interface Props {
+  id: string;
+  label: string;
+  walletAddress: Pick<WalletAddress, 'assetCode' | 'assetScale'>;
+  amount: string;
+  onChange: (amount: string, inputEl: HTMLInputElement) => void;
+  onError: (error: ErrorWithKeyLike) => void;
+  className?: string;
+  placeholder?: string;
+  errorMessage?: string;
+  readOnly?: boolean;
+  labelHidden?: boolean;
+  errorHidden?: boolean;
+  min?: number;
+  max?: number;
+  controls?: boolean;
+}
+
+export const InputAmount = ({
+  label,
+  id,
+  walletAddress,
+  amount,
+  className,
+  placeholder,
+  errorMessage,
+  onChange,
+  onError,
+  labelHidden,
+  errorHidden,
+  min = 0,
+  max,
+  readOnly,
+}: Props) => {
+  const currencySymbol = getCurrencySymbol(walletAddress.assetCode);
+  return (
+    <Input
+      id={id}
+      type="text"
+      inputMode="numeric"
+      label={labelHidden ? null : label}
+      aria-label={labelHidden ? label : undefined}
+      placeholder={placeholder}
+      className={className}
+      defaultValue={amount}
+      readOnly={readOnly}
+      addOn={<span className="text-weak">{currencySymbol}</span>}
+      errorMessage={errorHidden ? '' : errorMessage}
+      aria-invalid={errorHidden ? !!errorMessage : false}
+      required={true}
+      onKeyDown={allowOnlyNumericInput}
+      onBlur={(ev) => {
+        const input = ev.currentTarget;
+        const value = input.value;
+        if (value === amount && !input.required) {
+          return;
+        }
+        const error = validateAmount(value, walletAddress, min, max);
+        if (error) {
+          onError(error);
+        } else {
+          const amountValue = formatNumber(+value, walletAddress.assetScale);
+          input.value = amountValue;
+          onChange(amountValue, input);
+        }
+      }}
+    />
+  );
+};
+
+export function validateAmount(
+  value: string,
+  walletAddress: Pick<WalletAddress, 'assetCode' | 'assetScale'>,
+  min: number = 0,
+  _max?: number,
+): null | ErrorWithKeyLike {
+  if (!value) {
+    return errorWithKey('connectWallet_error_amountRequired');
+  }
+  const val = Number(value);
+  if (Number.isNaN(val)) {
+    return errorWithKey('connectWallet_error_amountInvalidNumber');
+  }
+  if (val <= min) {
+    return errorWithKey('connectWallet_error_amountMinimum', [
+      formatCurrency(min, walletAddress.assetCode, walletAddress.assetScale),
+    ]);
+  }
+  return null;
+}
+
+function allowOnlyNumericInput(ev: React.KeyboardEvent<HTMLInputElement>) {
+  if (
+    (!charIsNumber(ev.key) &&
+      ev.key !== 'Backspace' &&
+      ev.key !== 'Delete' &&
+      ev.key !== 'Enter' &&
+      ev.key !== 'Tab') ||
+    (ev.key === '.' && ev.currentTarget.value.includes('.'))
+  ) {
+    ev.preventDefault();
+  }
+}
