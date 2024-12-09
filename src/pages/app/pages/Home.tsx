920788ff664ef49fcb4600b95c48b7cdcdde590e
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-12-09 18:44:13 +0530

    clear up Home (remove state related code and links)

--- src/pages/app/pages/Home.tsx
+++ src/pages/app/pages/Home.tsx
@@ -1,31 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { ROUTES } from '@/app/App';
-import { useAppState } from '@/app/lib/store';
 
 export const Component = () => {
-  const state = useAppState();
-  return (
-    <div className="mx-auto max-w-2xl space-y-4 p-6">
-      <div className="flex items-center justify-between">
-        <h1 className="text-2xl">Home</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link
-                className="text-blue-500 underline"
-                to={ROUTES.POST_INSTALL}
-              >
-                Post install
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      <hr />
-
-      <pre>State: {JSON.stringify(state, null, 2)}</pre>
-    </div>
-  );
+  return <h1>Home</h1>;
 };
