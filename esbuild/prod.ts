2556512ae6ac6f517d1123d705f4bb23bc6cf86e
darian <darian@interledger.foundation>
2024-12-10 22:24:34 +0200

    loader treat .woff2 file as binary file

--- esbuild/prod.ts
+++ esbuild/prod.ts
@@ -24,7 +24,7 @@ export const getProdOptions = ({
     sourcemap: false,
     metafile: true,
     minify: true,
-    loader: { '.woff2': 'empty' },
+    loader: { '.woff2': 'binary' },
     plugins: getPlugins({ outDir, dev: false, target, channel }).concat([
       typecheckPlugin({ buildMode: 'readonly' }),
       preservePolyfillClassNamesPlugin({ outDir }),
