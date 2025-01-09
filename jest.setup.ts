de0db4381ccd19f8b249ed36e713f701eaaa00ea
darian <darian@interledger.foundation>
2025-01-09 14:14:39 +0200

    add defines mocks

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,4 +2,14 @@ import '@testing-library/jest-dom';
 
 import { chrome } from 'jest-chrome';
 
-Object.assign(global, { chrome: chrome, browser: chrome });
+jest.mock('./src/shared/defines', () => ({
+  LOG_LEVEL: 'info',
+  PERMISSION_HOSTS: { origins: [] },
+  ALLOWED_PROTOCOLS: [],
+  OPEN_PAYMENTS_REDIRECT_URL: 'https://webmonetization.org/welcome',
+}));
+
+Object.assign(global, {
+  chrome: chrome,
+  browser: chrome,
+});
