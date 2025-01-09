cedec52d47e90fc325bec14ce6dca18cf4e6c0e4
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-01-09 18:37:49 +0530

    lint fix

--- src/background/utils.ts
+++ src/background/utils.ts
@@ -10,21 +10,21 @@ import { INTERNAL_PAGE_URL_PROTOCOLS, NEW_TAB_PAGES } from './constants';
 import { notNullOrUndef } from '@/shared/helpers';
 import { OPEN_PAYMENTS_REDIRECT_URL } from '@/shared/defines';
 
-export const enum GrantResult {
+export enum GrantResult {
   GRANT_SUCCESS = 'grant_success',
   GRANT_ERROR = 'grant_error',
   KEY_ADD_SUCCESS = 'key_add_success',
   KEY_ADD_ERROR = 'key_add_error',
 }
 
-export const enum InteractionIntent {
+export enum InteractionIntent {
   CONNECT = 'connect',
   RECONNECT = 'reconnect',
   FUNDS = 'funds',
   UPDATE_BUDGET = 'update_budget',
 }
 
-export const enum ErrorCode {
+export enum ErrorCode {
   CONTINUATION_FAILED = 'continuation_failed',
   HASH_FAILED = 'hash_failed',
   KEY_ADD_FAILED = 'key_add_failed',
