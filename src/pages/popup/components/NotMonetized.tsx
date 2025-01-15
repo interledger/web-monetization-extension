ca5ca39c1de5718f7e8575c835f905ee37a7577b
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-01-15 17:06:27 +0530

    fix(bg/tabEvents): don't clear sessions on iframe navigation (#831)

--- src/pages/popup/components/NotMonetized.tsx
+++ src/pages/popup/components/NotMonetized.tsx
@@ -7,7 +7,12 @@ export const NotMonetized = ({ text }: { text: string }) => {
       <div className="flex-shrink-0">
         <WarningSign className="size-10 text-medium" />
       </div>
-      <h3 className="text-center text-lg leading-tight text-medium">{text}</h3>
+      <h3
+        className="text-center text-lg leading-tight text-medium"
+        data-testid="not-monetized-message"
+      >
+        {text}
+      </h3>
     </div>
   );
 };
