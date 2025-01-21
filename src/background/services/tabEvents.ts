9f68d391dd7d553cad34bac819600bcedd93f454
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-01-21 21:10:05 +0530

    fix(background): clear sessions on page reload (#851)

--- src/background/services/tabEvents.ts
+++ src/background/services/tabEvents.ts
@@ -87,7 +87,7 @@ export class TabEvents {
      * if loading and no url -> clear all sessions but not the overpaying state
      * if loading and url -> we need to check if state keys include this url.
      */
-    if (changeInfo.status === 'loading' && changeInfo.url) {
+    if (changeInfo.status === 'loading') {
       const url = tab.url ? removeQueryParams(tab.url) : '';
       const clearOverpaying = this.tabState.shouldClearOverpaying(tabId, url);
 
