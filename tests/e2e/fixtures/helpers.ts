4dd214d5e102acea603bebd9b4005aec0167a43f
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-02-11 15:22:32 +0530

    test(e2e): add tests for out of funds & add funds (#904)

--- tests/e2e/fixtures/helpers.ts
+++ tests/e2e/fixtures/helpers.ts
@@ -346,7 +346,7 @@ export class BrowserIntl {
     }
 
     for (const [key, { content }] of Object.entries(msg.placeholders)) {
-      const idx = Number(content.replace('$', ''));
+      const idx = Number(content.replace('$', '')) - 1;
       result = result.replaceAll(`$${key.toUpperCase()}$`, substitutions[idx]);
     }
     return result;
