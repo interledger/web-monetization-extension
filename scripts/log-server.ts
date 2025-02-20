7092dc4bba62e1bb53cb8aa16e4b35afa2d1812c
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-02-20 16:16:58 +0530

    nit: improve formatting a bit

--- scripts/log-server.ts
+++ scripts/log-server.ts
@@ -3,7 +3,7 @@ import {
   type IncomingMessage,
   type ServerResponse,
 } from 'node:http';
-import { formatWithOptions, styleText } from 'node:util';
+import { format, formatWithOptions, styleText } from 'node:util';
 import type { RemoteLoggerMessage } from '@/shared/logger';
 
 const PORT = process.env.LOG_SERVER_PORT || 8000;
@@ -31,9 +31,9 @@ const server = createServer(async (req, res) => {
       ),
       log.msg
         .map((e) =>
-          typeof e === 'object'
-            ? formatWithOptions({ colors: true }, '%o', e)
-            : formatWithOptions({ compact: true }, '%s', e),
+          typeof e === 'string'
+            ? format('%s', e)
+            : formatWithOptions({ colors: true }, '%o', e),
         )
         .join(' '),
       styleText(['italic', 'dim'], log.stackTrace[0]?.trim()),
