2a2a10c8f9e4d59151d1c4790ea80955d1df55d8
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-03-07 13:36:08 +0530

    on home page, sometimes user-action-required state is reached early
    Handle to ensure ProtectedRoute (triggers from home page) is accepted.

--- src/pages/popup/components/ErrorKeyRevoked.tsx
+++ src/pages/popup/components/ErrorKeyRevoked.tsx
@@ -105,7 +105,11 @@ const MainScreen = ({
   };
 
   return (
-    <m.div exit={{ opacity: 0 }} className="space-y-4 text-sm">
+    <m.div
+      exit={{ opacity: 0 }}
+      className="space-y-4 text-sm"
+      data-state="user-action-required"
+    >
       <div className="flex gap-2 rounded-md bg-error p-2">
         <WarningSign className="size-6 text-error" />
         <h3 className="text-base font-medium text-error">
