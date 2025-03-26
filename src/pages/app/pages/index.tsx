5858c242f8c341f346a8e149e8b9c2d80214064a
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-03-26 15:47:11 +0530

    perf: don't use Suspense/lazy as load looks slower

---
+++ src/pages/app/pages/index.tsx
@@ -0,0 +1,1 @@
+export { default as PostInstall } from './PostInstall';
---
+++ src/pages/popup/pages/index.tsx
@@ -0,0 +1,7 @@
+export { default as Home } from './Home';
+export { default as ConnectWallet } from './ConnectWallet';
+export { default as Settings } from './Settings';
+export { default as MissingHostPermission } from './MissingHostPermission';
+export { default as OutOfFunds } from './OutOfFunds';
+export { default as OutOfFundsAddFunds } from './OutOfFunds_AddFunds';
+export { default as ErrorKeyRevoked } from './ErrorKeyRevoked';
