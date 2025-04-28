594c08de27d01f47bd87f2c96370d53a753dc8f9
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-04-28 19:16:11 +0530

    nit: use interface extends instead of `type &`

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -40,7 +40,7 @@ export interface RecurringGrant extends GrantDetailsBase {
 }
 export type GrantDetails = OneTimeGrant | RecurringGrant;
 
-export type WalletInfo = WalletAddress & {
+export interface WalletInfo extends WalletAddress {
   /**
    * The (normalized) wallet URL provided by user. Sometimes, wallets URLs have
    * redirects, and in those cases, we want to preserve what user has provided.
@@ -48,7 +48,7 @@ export type WalletInfo = WalletAddress & {
    * @since Available only if wallet connected after this feature was released.
    */
   url?: string;
-};
+}
 
 export type ExtensionState =
   | never // just added for code formatting
