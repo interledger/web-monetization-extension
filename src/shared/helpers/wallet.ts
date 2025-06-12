a9ac350e376b80e0164eefae3d401b57955c1f96
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-06-12 14:42:18 +0530

    fix regex (as we already added `https://`)

--- src/shared/helpers/wallet.ts
+++ src/shared/helpers/wallet.ts
@@ -6,7 +6,7 @@ export function toWalletAddressUrl(s: string): string {
   if (s.startsWith('https://')) return s;
 
   const addr = s.replace(/^\$/, 'https://').replace(/\/$/, '');
-  if (/\/[^\/]*$/.test(addr)) {
+  if (/^https:\/\/.*\/[^\/].*$/.test(addr)) {
     return addr;
   }
   return `${addr}/.well-known/pay`;
