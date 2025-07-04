dd54a7435d0e0a085d5e49749397a6dfc7b04603
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-07-04 18:31:35 +0530

    don't keep Resources in git

--- src/safari/Web Monetization/Shared (App)/ViewController.swift
+++ src/safari/Web Monetization/Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "org.interledger.Web-Monetization.Extension"
+let extensionBundleIdentifier = "org.interledger.WebMonetization.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
