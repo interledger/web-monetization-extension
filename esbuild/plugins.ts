3d62e395c11fb0d61c2c665ecb448679b445dd24
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-07-04 15:49:17 +0530

    ignore safari specific files from firefox/chrome builds

--- esbuild/plugins.ts
+++ esbuild/plugins.ts
@@ -14,6 +14,7 @@ import {
   type BuildArgs,
   type WebExtensionManifest,
 } from './config';
+import { isNotNull } from '@/shared/helpers';
 
 const require = createRequire(import.meta.url);
 
@@ -79,6 +80,9 @@ export const getPlugins = ({
         },
       ],
       watch: dev,
+      globbyOptions: {
+        ignore: [target !== 'safari' ? '**/*safari*' : null].filter(isNotNull),
+      },
     }),
     processManifestPlugin({ outDir, dev, target, channel }),
   ];
