4b483cfc85b130cc10fe4be64bc8ae3b3b91b377
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-09-29 18:50:30 +0530

    popup/ConsentRequired: move all strings to `_locales/en/messages.json`

--- src/pages/popup/pages/ConsentRequired.tsx
+++ src/pages/popup/pages/ConsentRequired.tsx
@@ -1,27 +1,23 @@
-import { useMessage } from '@/popup/lib/context';
-import { Button } from '@/pages/shared/components/ui/Button';
 import React from 'react';
+import { useMessage, useTranslation } from '@/popup/lib/context';
+import { Button } from '@/pages/shared/components/ui/Button';
 import { WarningSign } from '@/pages/shared/components/Icons';
 
 export default () => {
+  const t = useTranslation();
   const message = useMessage();
   return (
     <div className="space-y-4 text-base" data-user-action="required">
       <div className="flex gap-2 rounded-md bg-error p-2">
         <WarningSign className="size-6 text-error" />
         <h3 className="text-base font-medium text-error">
-          Consent Required: Data Collection & Transmission
+          {t('consentRequired_text_title')}
         </h3>
       </div>
 
-      <p className="">
-        We want to be transparent about what data is shared and how it’s used.
-      </p>
+      <p className="">{t('consentRequired_text_subtitle')}</p>
 
-      <p className="pt-2">
-        To use the the Web Monetization Extension, we require your consent to
-        our data collection & transmission policy.
-      </p>
+      <p className="pt-2">{t('consentRequired_text_msg')}</p>
 
       <Button
         type="button"
@@ -29,7 +25,7 @@ export default () => {
           message.send('OPEN_APP', { path: '/post-install/consent' })
         }
       >
-        View Policy
+        {t('consentRequired_action_primary')}
       </Button>
     </div>
   );
