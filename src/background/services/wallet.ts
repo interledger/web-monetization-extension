0ef918ac08476f6644e0c46113e2771ebcf78fcc
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-10-17 20:15:05 +0530

    feat: support MMAON wallet (#1034)

--- src/background/services/wallet.ts
+++ src/background/services/wallet.ts
@@ -30,8 +30,7 @@ import {
 import { APP_URL } from '@/background/constants';
 import { bytesToHex } from '@noble/hashes/utils.js';
 import type { Cradle } from '@/background/container';
-import type { TabId } from '@/shared/types';
-import type { WalletAddress } from '@interledger/open-payments';
+import type { TabId, WalletInfo } from '@/shared/types';
 import type { Browser, Tabs } from 'webextension-polyfill';
 
 export class WalletService {
@@ -435,7 +434,7 @@ export class WalletService {
    * through the wallet's dashboard.
    */
   private async addPublicKeyToWallet(
-    walletAddress: WalletAddress,
+    walletAddress: WalletInfo,
     onTabOpen: (tabId: TabId) => void,
   ) {
     const keyAutoAdd = new KeyAutoAddService({
@@ -474,7 +473,7 @@ export class WalletService {
     }
   }
 
-  private async retryAddPublicKeyToWallet(walletAddress: WalletAddress) {
+  private async retryAddPublicKeyToWallet(walletAddress: WalletInfo) {
     let tabId: TabId | undefined;
     try {
       await this.addPublicKeyToWallet(walletAddress, (openedTabId) => {
