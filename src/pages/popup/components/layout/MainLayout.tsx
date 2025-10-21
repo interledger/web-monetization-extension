780586fca602a92873215f5578be5f28022fc4e2
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-21 15:16:53 +0530

    static fixture
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/pages/popup/components/layout/MainLayout.tsx
+++ src/pages/popup/components/layout/MainLayout.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { Header } from './Header';
 
 const Divider = () => {
-  return <div className="w-100 h-1 bg-divider-gradient" />;
+  return <div className="w-full h-px bg-divider-gradient" />;
 };
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     // biome-ignore lint/correctness/useUniqueElementIds: main is unique, as at top-level layout
-    <div className="flex w-popup flex-col gap-4 px-6 py-4" id="main">
+    <div
+      id="main"
+      className="flex w-popup min-h-[480px] min-w-[360px] max-h-[90vh] h-auto flex-col gap-4 px-6 py-4 overflow-hidden"
+    >
       <Header />
       <Divider />
-      <main className="flex flex-1 flex-col">{children}</main>
+      <main className="flex flex-1 flex-col overflow-y-auto">{children}</main>
     </div>
   );
-};
+}
