109c93f7c75e92bca3c8bc5548efa0bcafe8aff2
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2026-05-20 18:16:28 +0530

    fix(keyAutoAdd/gatehub): grab all addresses for ownership check

--- src/content/keyAutoAdd/lib/helpers/gatehub.ts
+++ src/content/keyAutoAdd/lib/helpers/gatehub.ts
@@ -1,4 +1,4 @@
-import { sleep } from '@/shared/helpers';
+import { ErrorWithKey, sleep } from '@/shared/helpers';
 
 export const getAuthToken = async (): Promise<string> => {
   let attempt = 0;
@@ -30,28 +30,70 @@ export const walletAddressUrlToId = (url: string) => {
   return pathname.match(/\/(\d+)/)![1];
 };
 
-export function getActiveWallet() {
-  let activeWallet: Record<string, unknown> | null = null;
-  try {
-    activeWallet = JSON.parse(
-      window.localStorage.getItem('activeWallet') || 'null',
-    );
-  } catch {}
+export async function getUserWallets() {
+  type GetWalletsResponse = Me<{
+    wallets: { address: string; walletType: string; enabled: boolean }[];
+  }>;
 
-  if (
-    typeof activeWallet === 'object' &&
-    activeWallet &&
-    typeof activeWallet.address === 'string' &&
-    activeWallet.address &&
-    activeWallet.walletType === 'Hosted'
-  ) {
-    return {
-      address: activeWallet.address,
-      walletType: activeWallet.walletType,
-    };
-  }
+  const data = await graphQlRequest<GetWalletsResponse>({
+    operationName: 'GetWallets',
+    variables: { filter: { includeDeleted: false } },
+    query: gql`
+      query GetWallets($filter: FilterWalletsInput, $walletType: WalletType) {
+        me {
+          wallets(filter: $filter, walletType: $walletType) {
+            address
+            walletType
+            enabled
+          }
+        }
+      }
+    `,
+  }).catch((error) => {
+    if (error instanceof GraphQlError) {
+      throw new ErrorWithKey('connectWalletKeyService_error_accountNotFound', [
+        error.message,
+      ]);
+    }
+    throw error;
+  });
+  return data.me.wallets;
+}
 
-  return null;
+export async function getUserPaymentPointers(wallet: {
+  address: string;
+  walletType: string;
+}) {
+  type GetUserPaymentPointersResponse = Me<{
+    wallets: { ilpPaymentPointers: { paymentPointerUrl: string }[] }[];
+  }>;
+
+  const data = await graphQlRequest<GetUserPaymentPointersResponse>({
+    operationName: 'GetUserPaymentPointers',
+    variables: {
+      address: wallet.address,
+      walletType: wallet.walletType,
+    },
+    query: gql`
+      query GetUserPaymentPointers($address: String!, $walletType: WalletType) {
+        me {
+          wallets(address: $address, walletType: $walletType) {
+            ilpPaymentPointers {
+              paymentPointerUrl
+            }
+          }
+        }
+      }
+    `,
+  }).catch((error) => {
+    if (error instanceof GraphQlError) {
+      throw new ErrorWithKey('connectWalletKeyService_error_accountNotFound', [
+        error.message,
+      ]);
+    }
+    throw error;
+  });
+  return data.me;
 }
 
 // For syntax highlighting
@@ -87,6 +129,8 @@ export async function graphQlRequest<R>(body: {
   }
   return json.data as R;
 }
+
+type Me<Data> = { me: Data };
 
 export class GraphQlError extends Error {
   public readonly errors: {
